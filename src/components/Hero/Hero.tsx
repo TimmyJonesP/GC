9dbab6d9b4bdeca5289e9cf6c6e7a1b3d0f2e9e1
Sandro <sandrocarioli43@gmail.com>
2024-06-12 23:47:25 -0300

    a

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
         alt="Home image"
         className="home h-full w-full max-h-max"
       />
-      <div className="gradiente w-full absolute bottom-[25px] md:bottom-0 h-[100px] rotate-180" />
+      <div className="gradiente w-full absolute bottom-0 h-[100px] rotate-180" />
     </header>
   );
 };
